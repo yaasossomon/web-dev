4eab037ab7277872a48084fd074c728f6f70656a
yaasossomon <student.as@youngsvilleacademy.org>
2026-04-29 12:29:25 -0400

    Update dark-mode-toggle.js

--- wdsub/dark-mode-toggle.js
+++ wdsub/dark-mode-toggle.js
@@ -1,26 +1,26 @@
 let darkModeTgl = document.getElementById("dark-mode-tgl");
 let onOffStatus = document.getElementById("on-off-status");
-let islight = true;
+let isLight = true;
 function mngMode(event) {
 
-  if(islight);
+  if(isLight);
   else ;
  
 
   
   // change background color to black if light mode, else white
-   if(islight) document.body.style.backgroundColor="black";
+   if(isLight) document.body.style.backgroundColor="black";
   else document.body.style.backgroundColor="white";
   
   // change text color to white if light mode, else black
   
-   if(islight) document.body.style.color = "white";
+   if(isLight) document.body.style.color = "white";
   else document.body.style.color = "black";
   
   
   // change text to "dark mode on"if light mode , else "dark mode off"
   
-   if(islight) onOffStatus.innerHTML="<p>Dark mode on</p>";
+   if(isLight) onOffStatus.innerHTML="<p>Dark mode on</p>";
   else onOffStatus.innerHTML="<p>Dark mode off</p>";
   
   // flip is light switch
